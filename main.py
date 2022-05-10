0061213b11961f5b4001fa0284572a5bf5f7e823
Hien Le <quang-hien.le.hsj@ssss.gouv.qc.ca>
2022-05-10 12:07:34 -0400

    Add markers to map from file, add example gpx

--- main.py
+++ main.py
@@ -16,20 +16,20 @@ get_locations("ilots.csv")
 
 df = pd.read_csv("ilots.csv")
 for index, row in df.iterrows():
-    print(f"{row['ilots']}, {row['adresse']}")
+    print(f"Add marker to map: {row['ilot']} -at- {row['adresse']}")
+    marker = geolocator.geocode(row['adresse'])
+    folium.Marker([marker.latitude, marker.longitude], 
+        popup=f"<strong>{row['ilot']}</strong><br>{row['adresse']}", 
+        tooltip=f"{row['ilot']}"
+    ).add_to(map)
 
-fermette = geolocator.geocode("1900, rue Le Ber, Montreal, Quebec")
-folium.Marker([fermette.latitude, fermette.longitude], popup=f"{fermette.address}", tooltip='Click Me!').add_to(map)
-
-goupillier = geolocator.geocode("4161, 54e Rue, Montreal, Quebec")
-folium.Marker([goupillier.latitude, goupillier.longitude], popup=f"{goupillier.address}", tooltip='Click Me!').add_to(map)
-
-plaza = geolocator.geocode("Metro Jean-Talon, Montreal, Quebec")
-folium.Marker([plaza.latitude, plaza.longitude], popup=f"{plaza.address}", tooltip='Click Me!').add_to(map)
-
-ecopivot = geolocator.geocode("400, Avenue Atlantic, Montreal, Quebec")
-folium.Marker([ecopivot.latitude, ecopivot.longitude], popup=f"{ecopivot.address}", tooltip='Click Me!', icon=folium.Icon(color="red", icon="info-sign")).add_to(map)
-
+#fermette = geolocator.geocode("1900, rue Le Ber, Montreal, Quebec")
+#folium.Marker([fermette.latitude, fermette.longitude], popup=f"{fermette.address}", tooltip='Click Me!').add_to(map)
+#goupillier = geolocator.geocode("4161, 54e Rue, Montreal, Quebec")
+#folium.Marker([goupillier.latitude, goupillier.longitude], popup=f"{goupillier.address}", tooltip='Click Me!').add_to(map)
+#plaza = geolocator.geocode("Metro Jean-Talon, Montreal, Quebec")
+#folium.Marker([plaza.latitude, plaza.longitude], popup=f"{plaza.address}", tooltip='Click Me!').add_to(map)
+#ecopivot = geolocator.geocode("400, Avenue Atlantic, Montreal, Quebec")
 #map
 map.save('index.html')
 print('\nDone!\n')
